fffde9a477e17bda19586817420bd9dbe0170dff
mahizaman490 <mahizaman490@gmail.com>
2023-09-01 20:31:16 +0600

    5th commit

--- index.js
+++ index.js
@@ -21,7 +21,51 @@ tabContainer.appendChild(div)
 const handleLoad = async (categoryId) => {
    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
    const data = await response.json()
-   console.log(data.data)
+   const cardContainer = document.getElementById("card-container")
+   data.data.slice(0,4).forEach((news) => {
+    console.log(news)
+const div = document.createElement("div")
+div.innerHTML = `
+
+
+<div class="card h-96 bg-base-100 shadow-xl">
+<figure><img src=${news.thumbnail} />
+
+</figure>
+<div class="card-body"> 
+<div class="flex">
+<div class="avatar">
+    <div class="w-16 rounded-full">
+      <img src=${news.authors[0].profile_picture} />
+    </div>
+  </div>
+<h2 class="card-title pl-3">
+${news.title.slice(0,14)}
+
+</h2>
+</div>
+<div class="flex items-center">
+
+<p class="pl-24 " id="">${news.authors[0].profile_name}</p>
+
+<div class="badge badge-primary mr-20 " id="">
+${news.authors[0].verified}
+
+
+</div>
+
+</div>
+<h3 class="pl-28"> <span id="">${news.others.views}</span> views</h3>
+</div>
+
+</div>
+
+
+`
+cardContainer.appendChild(div)
+
+ })
+
 }
 
 
